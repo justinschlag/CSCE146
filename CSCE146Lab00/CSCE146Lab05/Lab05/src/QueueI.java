0d2fd23f173aa8899e84735b4eaab0fd7db3c049
justinschlag <jschlag@email.sc.edu>
2025-03-17 11:14:15 -0400

    Delete CSCE146Lab00 directory

--- CSCE146Lab00/CSCE146Lab05/Lab05/src/QueueI.java
+++
@@ -1,9 +0,0 @@
-// Justin Schlag 2025
-// Queue interface for generic queue operations.
-
-public interface QueueI<T> {
-    void enqueue(T data);
-    T dequeue();
-    T peek();
-    void print();
-}
